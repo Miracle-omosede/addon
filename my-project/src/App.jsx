eee1df8fd170a60e5d7636e7c731b67738cf11d8
Miracle-omosede <miracleigbinoghene@gmail.com>
2023-02-05 18:06:57 +0100

    added Login and signUp form

---
+++ my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { React } from 'react'
+import Login from './pages/LoginPage/Login'
+import SignUp from './pages/signUpPage/SignUp'
+
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp/>
+    </div>
+  )
+}
+
+export default App
